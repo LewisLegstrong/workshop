e737f91e6d22662b4849ffcfce926e5973db40ae
Luis Duarte <luis.duarte@outlook.pt>
2025-03-20 16:36:53 +0000

    Added code to repo

---
+++ main.c
@@ -0,0 +1,10 @@
+#include "./inc/string.h"
+
+
+int main () {
+    const char phrase[16] = {"Hello World!"};
+    
+    printString(phrase);
+
+    return (0);
+}
---
+++ src/string.c
@@ -0,0 +1,5 @@
+#include "../inc/string.h"
+
+void printString (const char *string){
+    printf ("%s", string);
+}
